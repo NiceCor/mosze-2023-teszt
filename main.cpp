80a039373034094c252ce0c0023e079075961d77
NiceCor <96444242+NiceCor@users.noreply.github.com>
2022-09-19 14:14:30 +0200

    Javított kód

--- main.cpp
+++ main.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 
-constexpr int N_ELEMENTS = 100;
+int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //int* b, N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa' //lemaradt a vége <<std::endl;
-    for (int i = 0;) // lemaradt a for ciklus két eleme: i<=N_ELEMENTS; i++ 
+    int* b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+    for (int i = 1; i<=N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) //for ciklus középső eleménél hiányos
+    for (int i = 1; i<101; i++)
     {
-        std::cout << "Ertek:" //a vége hiányos: <<b[i]<<std::endl;
-    }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; //kezdőérték adás hiányzik és az átlag miatt legyen double
-    for (int i = 0; i < N_ELEMENTS, i++)
-    {
-        atlag += b[i] //pontosvessző
+        std::cout << "Ertek:"<< b[i] <<std::endl;
     }
-    atlag /= N_ELEMENTS; 
-    std::cout << "Atlag: " << atlag << std::endl; 
+    std::cout << "Atlag szamitasa: " << std::endl;
+    double atlag = 0;
+    for (int i = 1; i <= N_ELEMENTS; i++)
+    {
+        atlag = b[i] + atlag;
+    }
+    std::cout << "Atlag: " << atlag/N_ELEMENTS << std::endl;
     return 0;
 }
