40ac71abb0de0395545b31fb7a12a0ddd4b0cb78
NiceCor <96444242+NiceCor@users.noreply.github.com>
2022-09-19 14:10:11 +0200

    Hibák leírása

--- main.cpp
+++ main.cpp
@@ -4,23 +4,23 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //int* b, N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa' //lemaradt a vége <<std::endl;
+    for (int i = 0;) // lemaradt a for ciklus két eleme: i<=N_ELEMENTS; i++ 
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //for ciklus középső eleménél hiányos
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //a vége hiányos: <<b[i]<<std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag; //kezdőérték adás hiányzik és az átlag miatt legyen double
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] //pontosvessző
     }
-    atlag /= N_ELEMENTS;
-    std::cout << "Atlag: " << atlag << std::endl;
+    atlag /= N_ELEMENTS; 
+    std::cout << "Atlag: " << atlag << std::endl; 
     return 0;
 }
